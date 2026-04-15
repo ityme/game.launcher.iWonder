ce299953fe763cac654d26488ba83dec4c5d5303
ityme <ityme@outlook.com>
2026-04-15 16:49:20 +0800

    Enhance error handling in launcher scripts to ensure proper execution path and user guidance

--- lol-launcher-by-origin.bat
+++ lol-launcher-by-origin.bat
@@ -4,4 +4,13 @@ set "CLIENT_TYPE=origin"
 
 powershell -ExecutionPolicy Bypass -File "%~dp0core\lol-launcher.ps1" -CLIENT_TYPE "%CLIENT_TYPE%"
 
-exit
+if %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo [ENV ERROR]: can't find core file: lol-launcher.ps1
+    echo   Please run the .bat file from its original project folder.
+    echo     Do not move or copy it elsewhere!
+    echo.
+    pause
+) else (
+    exit
+)
